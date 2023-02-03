3c02888b35c3b75694bb92c13e9a294fadcdec2c
Cmyna <58068457+Cmyna@users.noreply.github.com>
2023-02-03 12:11:17 +0800

    add a profiling test for study

--- api/src/test/kotlin/study/performanceProfiling/PerformanceProfiling.kt
+++ api/src/test/kotlin/study/performanceProfiling/PerformanceProfiling.kt
@@ -1,6 +1,7 @@
 package study
 
 import org.junit.jupiter.api.Test
+import study.performanceProfiling.Tools.timeProfiling
 import java.io.ByteArrayInputStream
 import java.io.ByteArrayOutputStream
 import java.io.InputStream
@@ -268,34 +269,7 @@ class PerformanceProfiling {
         }.also {println("anonymous object cost: ${it.first/1000/1000} ms")}
     }
 
-    /**
-     * time profiling ensures that all object from fn() is created on heap (by holding these objects so that escape analysis will ignore them)
-     * also there is an duplicate operation to measure set object to array time cost and reduce this bias
-     */
-    private fun timeProfiling(times:Int, fn:()->Any):Pair<Long, Collection<Any>> {
-        val arr = arrayOfNulls<Any>(times)
-        var pointer = 0
-        var time:Long = 0
-        time -= System.nanoTime()
-        repeat(times) {
-            val res = fn()
-            arr[pointer] = res
-            pointer += 1
-        }
-        time += System.nanoTime()
 
-        val arr2 = arrayOfNulls<Any>(times)
-        pointer = 0
-        var bias:Long = -System.nanoTime()
-        repeat(times) {
-            arr[pointer] = arr[pointer]
-            pointer += 1
-        }
-        bias += System.nanoTime()
-        //println("static operation cost: ${staticCost/1000.0f/1000}ms")
-        val collection = setOf(arr, arr2)
-        return Pair(time-bias, collection)
-    }
 
 
     private fun directInherit(bytes:ByteArray, deque:Deque<Any>, inputStream: InputStream, outputStream: OutputStream):PR_TOP {
