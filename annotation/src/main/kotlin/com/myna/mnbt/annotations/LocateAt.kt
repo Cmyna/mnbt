77bb2aed35b453b66111eef5d10e7a63715a44b0
Cmyna <58068457+Cmyna@users.noreply.github.com>
2023-02-08 21:06:35 +0800

    change LocateAt annotation args
    separate argument "path" to two arguments: "encodePath" and "decodePath".

--- annotation/src/main/kotlin/com/myna/mnbt/annotations/LocateAt.kt
+++ annotation/src/main/kotlin/com/myna/mnbt/annotations/LocateAt.kt
@@ -11,7 +11,19 @@ const val IdTagCompound:Byte = 10
  * this annotation is used for class that declares relationship to a Tag object.
  * It declares the related path to Nbt Tree Structure
  *
- * @param path: the mnbt url related path
- * the path format is something like `./<tagName1>/<tagName2>/<targetTagName>`
+ * @param encodePath: the mnbt url relate path
+ * the path format is something like `./<tagName1>/<tagName2>/<targetTagName>`,
+ * it is not allowed to have any index format path segment in path (string like `#5`).
+ * Mnbt will throws [IllegalArgumentException] if there are some index format path segment in createTag process
+ * @param decodePath: the mnbt url relate path, if not specify this args, it will be regarded as same value to encode path.
+ * Different from [encodePath], index format path segment is allowed in path
  */
-annotation class LocateAt(val path:String)
+annotation class LocateAt(val encodePath:String, val decodePath:String = "") {
+
+    companion object {
+        fun getDecodePath(annotation: LocateAt):String {
+            return if (annotation.decodePath == "") annotation.encodePath
+            else annotation.decodePath
+        }
+    }
+}
