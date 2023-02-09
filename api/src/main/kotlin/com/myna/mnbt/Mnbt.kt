4565de19a9a89b198be649f993a5de4022549700
Cmyna <58068457+Cmyna@users.noreply.github.com>
2023-02-09 15:57:33 +0800

    document for Mnbt

--- api/src/main/kotlin/com/myna/mnbt/Mnbt.kt
+++ api/src/main/kotlin/com/myna/mnbt/Mnbt.kt
@@ -11,9 +11,12 @@ import com.myna.mnbt.experiment.ListTagCodec
 import com.myna.mnbt.experiment.OnBytesCodecProxy
 import com.myna.mnbt.reflect.MTypeToken
 import com.myna.mnbt.tag.AnyCompound
+import com.myna.mnbt.utils.UnsupportedProperty
 import java.io.InputStream
 import java.io.OutputStream
 import java.lang.NullPointerException
+
+//TODO: Exception analyse, refactoring and handling
 
 @Suppress("UnstableApiUsage")
 open class Mnbt {
@@ -34,9 +37,12 @@ open class Mnbt {
     )
 
     /**
-     * doc: TODO
+     * This options is experimental and may be removed in the future!
+     *
+     * Specifies that use an AdaptedInputStream that inherit and encapsulate all functionality from the original InputStream
+     * and optimize its performance by refactor its method code
      */
-    var autoAdaptedStream:Boolean = false
+    var adaptedInputStream:Boolean = false
 
     /**
      * set convert object that has nullable properties or not. in default the value is true
@@ -68,9 +74,10 @@ open class Mnbt {
      * if value is a Tag, then it will be encapsulated by an compound Tag with name passed in,
      * then serialize to bytes,
      * else value with value will first converted to Tag then serialized to bytes
-     * @param name name of nbt root tag
-     * @param value the value want to serialize
+     * @param name name of nbt root tag, see [varTopTagName]
+     * @param value the value want to serialize, see [paramJavaObject]
      * @param typeToken extra typeToken info for value (if ignore this parameter, TypeToken will use value.class as TypeTokenInfo)
+     * see [paramTypeToken]
      * @return a ByteArray stores data starts at 0 ends at ByteArray.size
      * @throws ConverterNullResultException if result of value->tag is null
      */
@@ -87,7 +94,7 @@ open class Mnbt {
      * deserialize nbt binary data from a ByteArray starts at pointer passed in
      * @param bytes the ByteArray stores nbt data
      * @param start pointer to the nbt data
-     * @param typeToken the returned value type converted from Nbt data,
+     * @param typeToken see [paramTypeToken]
      * if null it will return default type
      * @return a pair that first element stores root tag name, second one stores deserialized tag value
      */
@@ -101,7 +108,12 @@ open class Mnbt {
     }
 
     /**
-     * doc:TODO
+     * encode the object to binary nbt format, and write encoded result to an [OutputStream]
+     * @param name the root tag name, see [varTopTagName]
+     * @param value the object to encoded, see [paramJavaObject]
+     * @param typeToken see [paramTypeToken]
+     * @param outputStream see [paramBinOutputStream]
+     * @return return encode process success or not
      */
     fun <V:Any> toStream(name:String, value:V, typeToken: MTypeToken<out V>, outputStream:OutputStream):Boolean {
         if (value is Tag<*>) {
@@ -115,52 +127,71 @@ open class Mnbt {
     }
 
     /**
-     * doc:TODO
+     * decode binary nbt data from an [InputStream], and convert it to an object with specified object type
+     * @param typeToken @see [paramTypeToken]
+     * @param inputStream see [paramBinInputStream]
+     * @param converterIntent TODO
+     * @return see [returnFromTagResult]
      */
     fun <V:Any> fromStream(typeToken: MTypeToken<out V>, inputStream: InputStream, converterIntent: ToValueIntent?=null):Pair<String?, V>? {
-        val stream = if (autoAdaptedStream) AdaptedInputStream(inputStream) else inputStream
+        val stream = if (adaptedInputStream) AdaptedInputStream(inputStream) else inputStream
         val feedback = codecProxy.decode(userDecodeIntent(stream))
         return if (converterIntent!=null) converterProxy.toValue(feedback.tag, typeToken, converterIntent)
         else converterProxy.toValue(feedback.tag, typeToken)
     }
 
     /**
-     * doc:TODO
+     * encode a [Tag] as binary nbt format to an [OutputStream]
+     * @param tag the [Tag] want to encode
+     * @param outputStream see [paramBinOutputStream]
      */
     fun encode(tag:Tag<out Any>, outputStream: OutputStream) {
         codecProxy.encode(tag, userEncodeIntent(outputStream))
     }
 
     /**
-     * doc:TODO
+     * encode a [Tag] to an byte array
+     * @param tag the [Tag] want to encode
+     * @return a byte array stores encoded result starts at index 0, ends at byte array's end
      */
     fun encode(tag:Tag<out Any>):ByteArray {
         return (onByteCodecProxy.encode(tag, userOnBytesEncodeIntent()) as EncodedBytesFeedback).bytes
     }
 
     /**
-     * doc:TODO
+     * decode a tag from an [InputStream]
+     * @param inputStream see [paramBinInputStream]
+     * @return decoded [Tag]
      */
     fun decode(inputStream: InputStream):Tag<out Any> {
         return codecProxy.decode(userDecodeIntent(inputStream)).tag
     }
 
     /**
-     * doc:TODO
+     * decode a byte array
+     * @start specifies where the nbt binary data starts
+     * @return the decoded result
      */
     fun decode(bytes: ByteArray, start:Int):Tag<out Any> {
         return onByteCodecProxy.decode(userOnBytesDecodeIntent(bytes, start)).tag
     }
 
     /**
-     * doc: TODO
+     * convert a java object to [Tag]
+     * @param name see [varTopTagName]
+     * @param value see [paramJavaObject]
+     * @param typeToken see [paramTypeToken]
+     * @return convert result [Tag]
      */
     fun <V:Any> toTag(name:String, value:V, typeToken: MTypeToken<out V>):Tag<out Any>? {
         return converterProxy.createTag(name, value, typeToken, createTagUserIntent())
     }
 
     /**
-     * doc: TODO
+     * convert a [Tag] to an java object with specified type
+     * @param tag a [Tag] object
+     * @param typeToken see [paramTypeToken]
+     * @return see [returnFromTagResult]
      */
     fun <V:Any> fromTag(tag:Tag<out Any>, typeToken: MTypeToken<out V>):Pair<String?,V>? {
         return converterProxy.toValue(tag, typeToken)
@@ -212,6 +243,41 @@ open class Mnbt {
         this.converterProxy.registerToLast(mapTypeTagConverter)
 
         this.converterProxy.registerToLast(reflectiveConverter)
+    }
+
+    companion object {
+
+        /**
+         * a [paramJavaObject] class type token, presented by [MTypeToken]
+         */
+        private val paramTypeToken:Byte by UnsupportedProperty
+
+        /**
+         * the [InputStream] that contains Nbt binary data
+         */
+        private val paramBinInputStream:Byte by UnsupportedProperty
+
+        /**
+         * the [OutputStream] for writing Nbt binary data
+         */
+        private val paramBinOutputStream:Byte by UnsupportedProperty
+
+        /**
+         * a [String] value represent top tag name in an Nbt structure
+         */
+        private val varTopTagName:Byte by UnsupportedProperty
+
+        /**
+         * an Object or primitive value wants to encode or decode
+         */
+        private val paramJavaObject by UnsupportedProperty
+
+        /**
+         * a pair that first element stores the top tag name, second one
+         * stores the tag value converted to specified type (from [paramTypeToken])
+         */
+        private val returnFromTagResult by UnsupportedProperty
+
     }
 
 }
---
+++ api/src/main/kotlin/com/myna/mnbt/utils/UnsupportedProperty.kt
@@ -0,0 +1,10 @@
+package com.myna.mnbt.utils
+
+import kotlin.reflect.KProperty
+
+object UnsupportedProperty {
+    operator fun getValue(target:Any, property: KProperty<*>): Byte {
+        throw NotImplementedError()
+    }
+
+}
