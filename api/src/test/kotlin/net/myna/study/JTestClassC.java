83071d62fdf3c91328ae4f16088030707e7fcbdd
Cmyna <58068457+Cmyna@users.noreply.github.com>
2023-02-20 17:21:44 +0800

    delete package in api:test net.myna.study

--- api/src/test/kotlin/net/myna/study/JTestClassC.java
+++
@@ -1,7 +0,0 @@
-package net.myna.study;
-
-import net.myna.mnbt.annotations.LocateAt;
-
-@LocateAt(toTagPath = "sampleTag")
-public class JTestClassC {
-}
--- api/src/test/kotlin/net/myna/study/TestClassB.kt
+++
@@ -1,9 +0,0 @@
-package net.myna.study
-
-import net.myna.mnbt.annotations.LocateAt
-
-@LocateAt("testTag")
-class TestClassB {
-    val i:Int = 5
-    val str:String = "some string"
-}
